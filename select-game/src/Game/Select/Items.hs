8092a390c7970bba8af200b322150d9d915489ba
David Spies <dnspies@gmail.com>
2017-08-27 11:41:21 -0700

    Refactor InfoSet instances

--- select-game/src/Game/Select/Items.hs
+++ select-game/src/Game/Select/Items.hs
@@ -6,7 +6,13 @@
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Game.Select.Items where
+module Game.Select.Items
+    ( History
+    , History' (..)
+    , InfoSet
+    , InfoSet' (..)
+    , Items (..)
+    ) where
 
 import Data.DList (DList)
 import Data.Hashable (Hashable(..))
@@ -51,23 +57,22 @@ newtype ShowAllToShow f x = ShowAllToShow (f x)
 instance ShowAll f => Show (ShowAllToShow f x) where
   showsPrec d (ShowAllToShow x) = showsPrecAll d x
 
+determiningTuple
+  :: RemoveParam (InfoSet' phase history action) -> (PlayerIndex, Some phase, history)
+determiningTuple UInfoSet {uplayer, uphase, uhistory} = (uplayer, uphase, uhistory)
+
 instance
   ( EqAll phase
   , Eq history
   ) => Eq (RemoveParam (InfoSet' phase history action)) where
   -- Ignore options
-  (==) UInfoSet{uplayer=pl1, uphase=ph1, uhistory=h1}
-       UInfoSet{uplayer=pl2, uphase=ph2, uhistory=h2}
-    = (pl1, ph1, h1) == (pl2, ph2, h2)
+  (==) ui1 ui2 = determiningTuple ui1 == determiningTuple ui2
 instance
   ( OrdAll phase
   , Ord history
   ) => Ord (RemoveParam (InfoSet' phase history action)) where
   -- Ignore options
-  compare UInfoSet{uplayer=pl1, uphase=ph1, uhistory=h1}
-          UInfoSet{uplayer=pl2, uphase=ph2, uhistory=h2}
-    = compare (pl1, ph1, h1) (pl2, ph2, h2)
-
+  compare ui1 ui2 = compare (determiningTuple ui1) (determiningTuple ui2)
 
 deriving instance
   ( Show (action p)
@@ -91,7 +96,7 @@ instance
   , Hashable history
   ) => Hashable (RemoveParam (InfoSet' phase history action)) where
   -- Ignoring options
-  hashWithSalt d UInfoSet{uplayer, uphase, uhistory} = hashWithSalt d (uplayer, uphase, uhistory)
+  hashWithSalt d = hashWithSalt d . determiningTuple
 
 data History' reset reveal = History
   { begin   :: reset
